1f57b3a255f97d9c59fe68737ba6ced97f6f393e
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-07-23 01:41:29 +0530

    fixed env file things

--- communitysite/server/index.js
+++ communitysite/server/index.js
@@ -1,7 +1,9 @@
 // index.js
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
