533d0acd790af8b92dde93798f28632ba60a49ce
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-07-25 15:55:12 +0530

    fixing backend server unable to find debug module

--- communitysite/server/index.js
+++ communitysite/server/index.js
@@ -6,6 +6,19 @@ const morgan = require('morgan');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+  process.exit(1);
+});
+
 // Clerk is handled in individual route middlewares
 
 // Import database connection
